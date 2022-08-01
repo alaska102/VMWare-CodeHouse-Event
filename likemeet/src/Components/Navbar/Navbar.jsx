a8a0e284d36136aac37b13380c7307a36a5f80de
Hailey Garcia <haileyg102@berkeley.edu>
2022-08-01 09:44:26 -0700

    finished the navbar

--- likemeet/src/Components/Navbar/Navbar.jsx
+++ likemeet/src/Components/Navbar/Navbar.jsx
@@ -2,18 +2,22 @@ import * as React from 'react';
 import Logo from '../Logo/Logo';
 import './Navbar.css'; 
 import { AiTwotoneMessage, AiOutlineMenu } from 'react-icons/ai'
+import { IconContext } from 'react-icons' 
 
 export default function Navbar() {
     return(
+        <IconContext.Provider 
+            value={{color: '#d62828', size: 80}} >
         <div className="nav-bar">
             <div className="nav-content">
-                <Logo />
-                <div className="nav-options">
-                <AiTwotoneMessage />
-                <AiOutlineMenu />
+                <div className="logo"><Logo /></div>
+                <div className="container">
+                <AiTwotoneMessage/>
+                <AiOutlineMenu/>
                 </div>
             </div>
         </div>
+        </IconContext.Provider>
     );
     
 
